f235c6c3a580a89d3685018f60f22793f777f020
Xaerxess <xaerxess@gmail.com>
2010-11-12 00:12:01 +0100

    added zencoding

--- init.el
+++ init.el
@@ -44,6 +44,10 @@
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
+;; zencoding
+(require 'zencoding-mode)
+(add-hook 'sgml-mode-hook 'zencoding-mode) ;; Auto-start on any markup modes
+
 ;; Better auto completion for buffer switching and command execution.
 ;; ido-enable-flex-matching means that if the entered string does not match any buffer name, any buffer name containing the entered characters in the given sequence will match. 
 (require 'ido)
